41a36bbcad18286aaac7efc773350cb81a2c66d9
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2023-09-20 12:20:45 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,48 +1,17 @@
 from flask import Flask, jsonify, request
-from supabase_py import create_client, Client
 
-SUPABASE_URL = "YOUR_SUPABASE_URL"
-SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY"
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 app = Flask(__name__)
 
-# TODO Set up database for (Onramp, Nota)
-# TODO Set up AA wallet for each onramp (API-> register+creation, API-> Nota minting, API-> Nota recovery)
-# Having the claim process rely on us managing the wallet and giving them access?
-
-class Onramp:
-    def __init__(self, name, coverage_tier, historical_chargeback_data):
-        self.name = name
-        self.coverage_tier = coverage_tier
-        self.historical_chargeback_data = historical_chargeback_data
-
-    def __repr__(self):
-        return f'<Onramp {self.name}>'  
-
-class Nota:
-    def __init__(self, nota_id, onramp, payment_amount, payment_time, withdrawal_time):
-        self.nota_id = nota_id
-        self.onramp = onramp
-        self.payment_amount = payment_amount
-        self.payment_time = payment_time
-
-    def __repr__(self):
-        return f'<Nota {self.nota_id}>'
-
 # Onboarding endpoint
+
+
 @app.route('/register', methods=['POST'])
 def register_onramp():
     onramp_name = request.json.get('onrampName')
     coverage_tier = request.json.get('coverageTier')
-
-    # TODO need to store this somewhere. Also update it with onchain data
-    # Question: Will onramps share their historical data?
     historical_chargeback_data = request.json.get('historicalChargebackData')
 
     # TODO: Onboard the onramp and return credentials
-    onramp = Onramp(name=onramp_name, coverage_tier=coverage_tier, historical_chargeback_data=historical_chargeback_data)
-    data, count = supabase.table('onramp').insert({"id": 1, "name": "Denmark"}).execute()
 
     # For now, returning a stubbed response
     return jsonify({
@@ -51,6 +20,8 @@ def register_onramp():
     })
 
 # Transactions endpoint
+
+
 @app.route('/addTransaction', methods=['POST'])
 def add_transaction():
     payment_amount = request.json.get('paymentAmount')
@@ -65,6 +36,8 @@ def add_transaction():
     })
 
 # Recovery endpoint
+
+
 @app.route('/initiateRecovery', methods=['POST'])
 def initiate_recovery():
     nota_id = request.json.get('notaId')
@@ -78,4 +51,4 @@ def initiate_recovery():
 
 
 if __name__ == "__main__":
-    app.run(debug=True) # changing to 0.0.0 port opens it to the outside
+    app.run(debug=True)
