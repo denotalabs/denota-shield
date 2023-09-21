b4d4d4433f64b91bf1240ecf3ae0f5361aa3a73a
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2023-09-20 20:57:26 -0400

    initial code for initializing supabase, registering users, and authenticating endpoints

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
 from flask import Flask, jsonify, request
+from supabase_py import create_client, Client
+from functools import wraps
+import os
 
 app = Flask(__name__)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
 # Onboarding endpoint
 @app.route('/register', methods=['POST'])
 def register_onramp():
-    onramp_name = request.json.get('onrampName')
+    onramp_email = request.json.get('onrampName')
+    password = request.json.get('password')
     coverage_tier = request.json.get('coverageTier')
     historical_chargeback_data = request.json.get('historicalChargebackData')
 
-    # TODO: Onboard the onramp and return credentials
+    res = supabase.auth.sign_up({"email": onramp_email, "password": password, "options": { "data": {"coverage_tier": coverage_tier, "historical_chargeback_data": historical_chargeback_data}}})
 
-    # For now, returning a stubbed response
-    return jsonify({
-        "clientId": "stubbed_client_id",
-        "clientSecret": "stubbed_client_secret"
-    })
+    if res.error:
+        return jsonify({"error": res.error.message}), 400
+    return jsonify({"message": "User created successfully!"}), 201
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({"error": "Token is missing!"}), 401
+
+        response = supabase.auth.api.get_user(token)
+        if 'error' in response:
+            return jsonify({"error": "Invalid or expired token!"}), 401
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
 
 # Transactions endpoint
 @app.route('/nota', methods=['POST'])
+@token_required
 def add_nota():
     payment_amount = request.json.get('paymentAmount')
     payment_time = request.json.get('paymentTime')
@@ -34,6 +56,7 @@ def add_nota():
 
 # Recovery endpoint
 @app.route('/recovery', methods=['POST'])
+@token_required
 def initiate_recovery():
     nota_id = request.json.get('notaId')
     proof_of_chargeback = request.json.get('proofOfChargeback')
@@ -46,6 +69,7 @@ def initiate_recovery():
 
 # Recovery status endpoint
 @app.route('/recovery/<int:nota_id>', methods=['GET'])
+@token_required
 def get_recovery(nota_id):
     # TODO: pull the nota status
     return jsonify({
