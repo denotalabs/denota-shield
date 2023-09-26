a86d14bce8b494a1fabdb27052e730c7cde49221
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-09-26 17:29:49 -0400

    on chain nota minting

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, jsonify, request
-from supabase_py import create_client, Client
 from functools import wraps
-import dotenv
 
+import dotenv
+from eth_account import Account
+from flask import Flask, jsonify, request
+from supabase_py import Client, create_client
+from web3 import Web3
 
 env_path = ".env"
 url: str = dotenv.get_key(dotenv_path=env_path, key_to_get='SUPABASE_URL')
 key: str = dotenv.get_key(dotenv_path=env_path, key_to_get='SUPABASE_KEY')
 supabase: Client = create_client(url, key)
 
+INFURA_URL = 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'
+w3 = Web3(Web3.HTTPProvider(INFURA_URL))
+registrar = w3.eth.contract(address="", abi={})
+
+
 app = Flask(__name__)
 
 # Onboarding endpoint
+
+
 @app.route('/register', methods=['POST'])
 def register_onramp():
     onramp_email = request.json.get('email')
@@ -23,22 +32,26 @@ def register_onramp():
     # Input validation
     if not all([onramp_name, onramp_email, password, coverage_tier]):
         return jsonify({"error": "Required fields are missing"}), 400
-    
-    res = supabase.auth.sign_up(onramp_email, password) # This returns a dict with the user's id
+
+    # This returns a dict with the user's id
+    res = supabase.auth.sign_up(onramp_email, password)
     if res is None:
         return jsonify({"error": "User already exists"}), 400
     status_code = res.get("status_code")
     if (status_code != 200) and (status_code != 201):
         return jsonify({"error": status_code}), status_code
-    
-    # TODO generate user's web3 wallet then save it to the database
+
+    # generate user's web3 wallet then save it to the database
+    new_account = Account.create()
+    private_key = new_account.privateKey.hex()
 
     # add to user table
     user_data = {
         "id": res.get("id"),
         "name": onramp_name,
         "coverage_tier": coverage_tier,
-        "historical_chargeback_data": historical_chargeback_data
+        "historical_chargeback_data": historical_chargeback_data,
+        "private_key": private_key
     }
 
     res = supabase.table("User").insert(user_data).execute()
@@ -47,6 +60,7 @@ def register_onramp():
         return jsonify({"error": status_code}), status_code
 
     return f"Registration successful: {status_code}", status_code
+
 
 def token_required(f):
     @wraps(f)
@@ -73,51 +87,80 @@ def onramp_signin():
     # Input validation
     if not all([onramp_email, password]):
         return jsonify({"error": "Required fields are missing"}), 400
-    
-    session = supabase.auth.sign_in(onramp_email, password) # This returns a dict with the user's id
+
+    # This returns a dict with the user's id
+    session = supabase.auth.sign_in(onramp_email, password)
     status_code = session.get("status_code")
     if (status_code != 200) and (status_code != 201):
         return jsonify({"error": status_code}), status_code
-    
+
     return session.get("access_token"), status_code
 
 # Transactions endpoint
+
+
 @app.route('/nota', methods=['POST'])
 @token_required
 def add_nota():
-    res = supabase.auth.api.get_user(request.headers.get('Authorization')) # TODO This is a duplicated call for now (decorator and here)
-    
+    # TODO This is a duplicated call for now (decorator and here)
+    res = supabase.auth.api.get_user(request.headers.get('Authorization'))
+
     user_id = res.get("id")
     payment_amount = request.json.get('paymentAmount')
     risk_score = request.json.get('riskScore')
-    
+
     # Ensure the required parameters are provided
     if not all([user_id, payment_amount, risk_score]):
         return jsonify({"error": "Required parameters missing!"}), 400
-    
-    # TODO: Mint nota NFT using web3 wallet
-    
+
+    # Mint nota NFT using web3 wallet
+    private_key = res.get("private_key")
+    mint_onchain_nota(private_key, "", payment_amount, risk_score)
+
     nota_data = {
         "user_id": user_id,
         "payment_amount": payment_amount,
         "risk_score": risk_score,
-        "recovery_status": 0 # 0 = not initiated, 1 = pending, 2 = completed
+        "recovery_status": 0  # 0 = not initiated, 1 = pending, 2 = completed
     }
-    
-    res = supabase.table("Nota").insert(nota_data).execute() # Sanitize input (don't allow duplicate minting, etc.)
+
+    # Sanitize input (don't allow duplicate minting, etc.)
+    res = supabase.table("Nota").insert(nota_data).execute()
     status_code = res.get("status_code")
     if (status_code != 200) and (status_code != 201):
         return jsonify({"error": status_code}), status_code
-    
+
     notas = res.get("data")
     if len(notas) > 1:
         raise Exception("More than one nota was created")
-    
+
     nota_id = notas[0].get("id")
     if nota_id is None:
         return jsonify({"error": "Failed to create nota"}), 400
 
-    return jsonify({"notaId": nota_id}), status_code #response.data["id"]
+    return jsonify({"notaId": nota_id}), status_code  # response.data["id"]
+
+
+def mint_onchain_nota(key, address, payment_amount, risk_score):
+    risk_fee = (payment_amount/10000)*risk_score
+    payload = w3.eth.encodeABI(
+        ["address", "uint256", "unit256"], ["", 1000, 50])
+    transaction = registrar.functions.mint("token", 0, risk_fee, "coverageModule", "coverageModule", payload).buildTransaction({
+        'chainId': 1,  # For mainnet
+        'gas': 2000000,  # Estimated gas, change accordingly
+        'gasPrice': w3.toWei('20', 'gwei'),
+        'nonce': w3.eth.getTransactionCount(address)
+    })
+    # Sign the transaction
+    signed_txn = w3.eth.account.signTransaction(transaction, key)
+
+    # Send the transaction
+    txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+    # Wait for the transaction receipt
+    receipt = w3.eth.waitForTransactionReceipt(txn_hash)
+
+    return receipt['transactionHash'].hex()
 
 
 # Recovery endpoint
@@ -125,14 +168,17 @@ def add_nota():
 @token_required
 def initiate_recovery():
     user = supabase.auth.api.get_user(request.headers.get('Authorization'))
-    
+
     user_id = user["id"]
     nota_id = request.json.get('notaId')
 
-    notas = supabase.table("Nota").select("*").eq("id", str(nota_id)).eq("user_id", str(user_id)).execute() # Limit 1?
+    # TODO: Initiate recovery onchain
+
+    notas = supabase.table("Nota").select(
+        "*").eq("id", str(nota_id)).eq("user_id", str(user_id)).execute()  # Limit 1?
     if notas is None:
         return jsonify({"error": "Doesn't exist or not authorized"}), 400
-    
+
     nota = notas.get("data")
 
     if len(nota) > 1:
@@ -152,25 +198,26 @@ def initiate_recovery():
     return jsonify({"message": "success"}), status_code
 
 # Recovery status endpoint
+
+
 @app.route('/recovery/<int:nota_id>', methods=['GET'])
 @token_required
 def get_recovery(nota_id):
     user = supabase.auth.api.get_user(request.headers.get('Authorization'))
     user_id = user["id"]
-    
-    notas = supabase.table("Nota").select("*").eq("id", str(nota_id)).eq("user_id", str(user_id)).execute() # Limit 1?
+
+    notas = supabase.table("Nota").select(
+        "*").eq("id", str(nota_id)).eq("user_id", str(user_id)).execute()  # Limit 1?
     if notas is None:
         return jsonify({"error": "Doesn't exist or not authorized"}), 400
-    
+
     nota = notas.get("data")
     if len(nota) > 1:
         raise Exception("More than one nota was created")
     nota = nota[0]
-    
+
     return jsonify({"status": nota["recovery_status"]})
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
-
-
