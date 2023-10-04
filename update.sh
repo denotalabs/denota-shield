313d0acc846d5dac83870f732fddc955aa7a032b
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-10-03 20:52:28 -0400

    comments

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git pull origin main  # or master, depending on your default branch
+git pull origin main  
 
-# Restart your Flask app
-# This could be done using systemd, Supervisor, or other methods depending on your setup
 sudo systemctl restart shield
